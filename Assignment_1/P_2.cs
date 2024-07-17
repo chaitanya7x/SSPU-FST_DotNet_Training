8c7b8c24b035d4bbc6299f707d11db5293a901f6
chaitanya7x <chaitanyamm.it_ioe@bkc.met.edu>
2024-07-18 00:34:30 +0530

    Commit

---
+++ Assignment_1/P_2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    public delegate void Operation(int a, int b);
+    class P_2
+    {
+    }
+}
